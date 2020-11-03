519cf5e73ef9d684181900882a2622d4d572a565
Geulmaster <eyal.geulayev@gmail.com>
2020-11-03 23:44:30 +0200

    Get host from client

--- app/index.py
+++ app/index.py
@@ -7,6 +7,7 @@ class Parser:
         group = parser.add_mutually_exclusive_group()
         group.add_argument("-v", "--verbose", action="store_true")
         group.add_argument("-q", "--quiet", action="store_true")
+        group.add_argument("-ch", "--host", help="Set host target")
         parser.add_argument("run", nargs='?', help='Run command')
         parser.add_argument("type", nargs='?', help="The load tests web request type")
         args = parser.parse_args()
@@ -15,8 +16,8 @@ class Parser:
 parser = Parser()
 parser.run_arguments()
 
-if args.quiet:
-    print("Done") # Will be imported from core.flow
+if args.host:
+    print(args.host) # Will change the configuration file value
 elif args.type:
     print(f"Type is {args.type}")
 else:
