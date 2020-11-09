8c8815e596db5905454a94d9fd2692c792625ee0
Geulmaster <eyal.geulayev@gmail.com>
2020-11-10 00:02:16 +0200

    add run_time option

--- app/core/flow.py
+++ app/core/flow.py
@@ -1,4 +1,5 @@
 import subprocess
+from time import sleep
 from selenium import webdriver
 from Loado.app.load_tests.get import Config
 from Loado.app.core.selectors import locust_selectors as sl
@@ -17,5 +18,7 @@ def get():
         driver.find_element_by_id(sl.total_users_id).send_keys(configuration.users)
         driver.find_element_by_id(sl.spawn_rate_id).send_keys(configuration.spawn_rate)
         driver.find_element_by_xpath(sl.swarming_btn_xpath).click()
+        sleep(configuration.run_time)
+        driver.find_element_by_class_name(sl.stop_btn_class).click()
     except:
         print("One or more of the credentials is incorrect!")
--- app/core/selectors/locust_selectors.py
+++ app/core/selectors/locust_selectors.py
@@ -1,3 +1,4 @@
 total_users_id = "user_count"
 spawn_rate_id = "spawn_rate"
 swarming_btn_xpath = '//*[@id="swarm_form"]/button'
+stop_btn_class = "stop-button"
