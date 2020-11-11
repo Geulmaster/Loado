5c10d552f8227c62d523e1c212ca7767db2030e6
Geulmaster <eyal.geulayev@gmail.com>
2020-11-11 23:41:58 +0200

    Apply time configuration field name's change

--- app/load_tests/__init__.py
+++ app/load_tests/__init__.py
@@ -6,18 +6,18 @@ class Config:
 
     def __init__(self, host = "localhost", port = "80",
      route = "/", users = str(5), spawn_rate = str(10),
-      run_time = str(30)):
+      time = str(30)):
         if configuration:
             self.host = configuration["Load Testing"]["host"]
             self.port = configuration["Load Testing"]["port"]
             self.route = configuration["Load Testing"]["route"]
             self.users = configuration["Load Testing"]["users"]
             self.spawn_rate = configuration["Load Testing"]["spawn_rate"]
-            self.run_time = configuration["Load Testing"]["run_time"]
+            self.time = configuration["Load Testing"]["time"]
         else:
             self.host = host
             self.port = port
             self.route = route
             self.users = users
             self.spawn_rate = spawn_rate
-            self.run_time = run_time
+            self.time = time
