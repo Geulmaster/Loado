f49491aea11f07d718eb772b1c1fce961c2cd0c3
Geulmaster <eyal.geulayev@gmail.com>
2020-12-03 22:51:18 +0200

    Add some doc strings in flow

--- app/core/flow.py
+++ app/core/flow.py
@@ -1,6 +1,7 @@
 import subprocess
 from pandas import DataFrame
 from time import sleep
+from json import dumps, loads
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,12 +13,18 @@ configuration = Config()
 options = webdriver.ChromeOptions()
 
 def export_results(result):
+    """
+    Empty results file and write new results to it
+    """
     res_file = open("results.txt", "w")
     res_file.close()
     with open("results.txt", "w") as results_file:
         results_file.write(result)
 
 def beautify_results():
+    """
+    Generates list of list for pandas.DataFrame
+    """
     with open("results.txt", "r") as results_file:
         file_content = results_file.readlines()
     del file_content[0]
@@ -31,8 +38,11 @@ def beautify_results():
 results = beautify_results()
 
 def get_results():
+    """
+    Return results table from locust's UI
+    """
     columns_list = []
-    columns = driver.find_elements_by_class_name("stats_label")
+    columns = driver.find_elements_by_class_name(sl.stats_label_class)
     for column in columns:
         columns_list.append(column.text)
     formatted_columns = [x for x in columns_list if x]
@@ -40,7 +50,16 @@ def get_results():
     print(table)
     return table
 
+def convert_results_to_json():
+    result = get_results().to_json(orient="split")
+    parsed_result = loads(result)
+    results_json_file = dumps(parsed_result, indent=4)
+    print(results_json_file)
+    
 def general_flow(type):
+    """
+    Runs load tests accordint to type
+    """
     global driver
     driver = webdriver.Chrome(options=options)
     run = subprocess.Popen(["locust", "-f", f"./load_tests/{type}.py", "--host", f"http://{configuration.host}:{configuration.port}"])
@@ -53,9 +72,10 @@ def general_flow(type):
         driver.find_element_by_xpath(sl.swarming_btn_xpath).click()
         sleep(int(configuration.time))
         driver.find_element_by_class_name(sl.stop_btn_class).click()
-        results = driver.find_element_by_id("stats").text
+        results = driver.find_element_by_id(sl.results_id).text
         export_results(results)
         get_results()
+        convert_results_to_json()
     except:
         print("One or more of the credentials is incorrect")
     driver.close()
