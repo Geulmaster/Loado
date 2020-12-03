b0ba82b2d2dbe1ef8ec7ad6fb50831761f0634cd
Geulmaster <eyal.geulayev@gmail.com>
2020-12-03 22:51:53 +0200

    Move selectors from flow to locust_selectors

--- app/core/selectors/locust_selectors.py
+++ app/core/selectors/locust_selectors.py
@@ -2,3 +2,5 @@ total_users_id = "user_count"
 spawn_rate_id = "spawn_rate"
 swarming_btn_xpath = '//*[@id="swarm_form"]/button'
 stop_btn_class = "stop-button"
+stats_label_class = "stats_label"
+results_id = "stats"
