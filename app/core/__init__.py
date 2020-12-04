15c80e73e3e277e88448fe499978b7f3387f9b9e
Geulmaster <eyal.geulayev@gmail.com>
2020-12-04 15:56:05 +0200

    Clean flow.py

---
+++ app/core/__init__.py
@@ -0,0 +1,23 @@
+
+def export_results(result):
+    """
+    Empty results file and write new results to it
+    """
+    res_file = open("results.txt", "w")
+    res_file.close()
+    with open("results.txt", "w") as results_file:
+        results_file.write(result)
+
+def beautify_results():
+    """
+    Generates list of list for pandas.DataFrame
+    """
+    with open("results.txt", "r") as results_file:
+        file_content = results_file.readlines()
+    del file_content[0]
+    values_list_of_list = []
+    for line in file_content:
+        stripped_line = line.strip()
+        line_list = stripped_line.split()
+        values_list_of_list.append(line_list)
+    return values_list_of_list
--- app/core/flow.py
+++ app/core/flow.py
@@ -8,32 +8,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Loado.app.load_tests.get import Config
 from Loado.app.core.selectors import locust_selectors as sl
+from Loado.app.core import export_results, beautify_results
 
 configuration = Config()
 options = webdriver.ChromeOptions()
-
-def export_results(result):
-    """
-    Empty results file and write new results to it
-    """
-    res_file = open("results.txt", "w")
-    res_file.close()
-    with open("results.txt", "w") as results_file:
-        results_file.write(result)
-
-def beautify_results():
-    """
-    Generates list of list for pandas.DataFrame
-    """
-    with open("results.txt", "r") as results_file:
-        file_content = results_file.readlines()
-    del file_content[0]
-    values_list_of_list = []
-    for line in file_content:
-        stripped_line = line.strip()
-        line_list = stripped_line.split()
-        values_list_of_list.append(line_list)
-    return values_list_of_list
 
 results = beautify_results()
 
